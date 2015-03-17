8c66e6b3303e03c0290864ac520223cec440c53f
ehoogsto <evelinekeh@gmail.com>
2015-03-17 20:29:18 +0100

    added routes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 App = Ember.Application.create();
+
+App.Router.map(function(){
+    this.resource('dashboard');
+    this.resource('drones');
+    this.resource('assignments');
+    this.resource('basestations');
+    this.resource('users');
+});
