d3e2d4d15caae56641a3d99ed2f7f8a71c646069
ehoogsto <evelinekeh@gmail.com>
2015-03-24 16:58:24 +0100

    building table with dummy data

--- js/app.js
+++ js/app.js
@@ -14,3 +14,27 @@ $(document).ready(function() {
         $(this).addClass('active');
     });
 });
+
+App.MyTableComponent = Ember.Component.extend({
+    columns: ['test', '#']
+});
+
+App.DronesRoute = Ember.Route.extend({
+    model: function(){
+        return drones
+    }
+});
+
+App.DronesController = Ember.ObjectController.extend({
+    columns : ['#','Name','Status','Actions']
+});
+
+var drones = [{
+    id: '1',
+    name: "bebop",
+    status: 'Available'
+}, {
+    id: '2',
+    name: "AR drone 2",
+    status: 'In flight'
+}]
