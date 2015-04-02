48cd2a2bfa88bd6d333daa8772e4623fa6ea0986
Matthias Verstraete <mmaverst.verstraete@ugent.be>
2015-04-02 18:13:42 +0200

    fix connection with socket

--- js/controllers/appcontroller.js
+++ js/controllers/appcontroller.js
@@ -4,10 +4,28 @@
 
 App.AppController = Ember.Controller.extend({
 
+    notification : "",
+    notificationIsError : function() {
+        return this.get('notification') !== "";
+    }.property('notification'),
+
+    init : function() {
+        this._super();
+
+        var self = this;
+        App.currentSocketManager.register("notification",null, function(data) {
+            self.set('notification', data.message);
+        });
+    },
+
     actions : {
         logout: function(){
             App.AuthManager.logout();
             this.transitionToRoute('login');
+        },
+
+        dismiss : function() {
+            this.set('notification', "");
         }
     }
 
