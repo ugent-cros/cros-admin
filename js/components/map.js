67888616d43d66108a7787c6359080b31556fddf
Matthias Verstraete <mmaverst.verstraete@ugent.be>
2015-04-02 18:42:57 +0200

    maps displaying actual location data

--- js/components/map.js
+++ js/components/map.js
@@ -1,12 +1,27 @@
 App.MyMapComponent = Ember.Component.extend({
-	didInsertElement : function(){
+	marker : null,
+    map : null,
+
+    updateMarker : function() {
+        var loc = this.get('location');
+        this.get('marker').setLatLng(loc);
+    }.observes('location'),
+
+    updateMap : function() {
+        var loc = this.get('location');
+        this.get('map').setView(loc);
+    }.observes('location'),
+
+    didInsertElement : function(){
         var self = this;
-        var map = L.map(self.mapName).setView([51.505, -0.09], 13);
+        var map = L.map(self.mapName).setView(this.get('location'), 13);
 
         L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery � <a href="http://mapbox.com">Mapbox</a>',
             maxZoom: 18
         }).addTo(map);
+
+        this.set('map',map);
 
         var customIcon = L.icon({
             iconUrl: 'img/marker-icon.png',
@@ -19,12 +34,12 @@ App.MyMapComponent = Ember.Component.extend({
             popupAnchor:  [0, -41] // point from which the popup should open relative to the iconAnchor
         });
 
-        var marker = L.marker([51.5, -0.09], {icon: customIcon}).addTo(map);
-        marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+        var marker = L.marker(this.get('location'), {icon: customIcon}).addTo(map);
+        marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
+        this.set('marker',marker);
 
         // TODO: fix ugly hack
         setTimeout(function() {
-            console.log("re-evaluating size");
            map.invalidateSize();
         }, 500);
     }
