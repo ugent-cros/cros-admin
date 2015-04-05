50ba36c6f0166a499d8fa683b3f92328dc3a41ad
Matthias Verstraete <mmaverst.verstraete@ugent.be>
2015-04-05 13:54:28 +0200

    login load fix

--- js/controllers/loginController.js
+++ js/controllers/loginController.js
@@ -2,6 +2,12 @@ App.LoginController = Ember.Controller.extend({
   
 	emailError : "",
 	passwordError : "",
+    hasEmailError : function() {
+        return this.get('emailError') !== "";
+    }.property('emailError'),
+    hasPasswordError : function() {
+        return this.get('passwordError') !== "";
+    }.property('passwordError'),
 	
 	emailInput : "",
 	passwordInput : "",
@@ -11,13 +17,18 @@ App.LoginController = Ember.Controller.extend({
 
 			var result = App.AuthManager.login(this, this.get('emailInput'), this.get('passwordInput'));
             var self = this;
-            result.then(function(success) {
-
-                if (success) {
-                    self.transitionToRoute('dashboard');
+            result.then(function() {
+                self.transitionToRoute('dashboard');
+            },function(data) {
+                if (data.status == 401) {
+                    self.set('emailError', "");
+                    self.set('passwordError', "the password was incorrect");
                 } else {
-                    self.transitionToRoute('login');
+                    self.set('emailError', data.responseJSON.emailAddress || "");
+                    self.set('passwordError', data.responseJSON.password || "");
                 }
+
+                NProgress.done();
 
             });
 		}
