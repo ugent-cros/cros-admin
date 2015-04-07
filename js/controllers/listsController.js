3fb564daef8428c895c8057ef1bfd5b87c0336df
ehoogsto <evelinekeh@gmail.com>
2015-04-07 14:38:52 +0200

    refresh after delete

--- js/controllers/listsController.js
+++ js/controllers/listsController.js
@@ -2,20 +2,30 @@
  * Created by Eveline on 5/04/2015.
  */
 App.ListSuperController = Ember.Controller.extend({
+
+    loadPage: function(search, page, perPage){
+        var self = this;
+        var elementClass = self.get('element');
+        var searchField = self.get('searchField');
+        var params = {pageSize : perPage, page : (page-1), total: true};
+        params[searchField] = search;
+        this.customAdapter.find(elementClass, null, null, params).then(function(data){
+            self.set('model',data);
+            return data;
+        });
+    },
+
     //Actions
     actions: {
         delete: function (id) {
-            var el = this.get('element');
-            this.customAdapter.remove(el, id);
+            var elementClass = this.get('element');
+            this.customAdapter.remove(elementClass, id);
+            this.loadPage(null,1,2);
         },
 
         getPage: function (search, page, perPage){
-            var self = this;
-            var element = self.get('element');
-            this.customAdapter.find(element, null, null, {name: search, pageSize : perPage, page : (page-1), total: true}).then(function(data){
-                self.set('model',data);
-                return data;
-            });
+            this.loadPage(search,page,perPage);
+            //todo: if total = 0
         }
     }
 });
