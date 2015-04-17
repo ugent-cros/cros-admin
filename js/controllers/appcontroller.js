c54a39e3776691ecd3838b9c7d91669708326385
unknown <benjamin.twechuizen@ugent.be>
2015-04-17 14:03:01 +0200

    Notifications added

--- js/controllers/appcontroller.js
+++ js/controllers/appcontroller.js
@@ -4,21 +4,27 @@
 
 App.AppController = Ember.Controller.extend({
 
-    notification : "",
-    notificationIsError : function() {
-        return this.get('notification') !== "";
-    }.property('notification'),
+    notification : {},
+	
+	notificationType: function() {
+		if(this.get('notification').message) {
+			$('#notificationSlider').slideDown('slow');
+			return 'alert alert-success';
+		}
+		return '';
+	}.property('notification'),
 
     init : function() {
         this._super();
+		this.registerForEvents();
 
         var self = this;
         if(this.socketManager) {
             this.socketManager.register("notification",null, function(data) {
                 if (data.action === "clear") {
-                    self.set('notification', "");
+                    self.set('notification', {});
                 } else {
-                    self.set('notification', data.message);
+                    self.set('notification', { message: message, link:'assignment', id: id });
                 }
             });
         }
@@ -32,8 +38,47 @@ App.AppController = Ember.Controller.extend({
         },
 
         dismiss : function() {
-            this.set('notification', "");
+            this.set('notification', {});
+			$('#notificationSlider').hide();
         }
-    }
-
+    },
+	
+	registerForEvents: function() {
+		var self = this;
+		this.socketManager.register("droneAssigned", 0, "application", function(data, id) {
+			self.handleEvent(data, id, function(data, id) {
+				self.adapter.find('drone', data.assignedDroneID).then(function(data) {
+					var message = data.name + ' got assigned to assignment #' + id;
+					self.set('notification', { message: message });
+				});
+			});
+		});
+		this.socketManager.register("assignmentStarted", 0, "application", function(data, id) {
+			self.handleEvent(data, id, function(data, id) {
+				var message = 'Started assignment #' + id;
+				self.set('notification', { message: message });
+			});
+		});
+		this.socketManager.register("assignmentProgressChanged", 0, "application", function(data, id) {
+			self.handleEvent(data, id, function(data, id) {
+				self.adapter.find('assignment', id).then(function(assignment){
+					var message = assignment.assignedDrone.name + ' reached the next checkpoint (' + data.progress + ' of ' + assignment.route.length + ')';
+					self.set('notification', { message: message });
+				});
+			});
+		});
+		this.socketManager.register("assignmentCompleted", 0, "application", function(data, id) {
+			self.handleEvent(data, id, function(data, id) {
+				var message = 'Completed assignment #' + id;
+				self.set('notification', { message: message });
+			});
+		});
+	},
+	
+	handleEvent: function(data, id, func) {
+		$('#notificationSlider').slideUp('slow', function() {
+			func(data, id);
+			$('#notificationSlider').slideDown('slow');
+		});
+	}
 });
