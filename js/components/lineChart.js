284e55c47c0b4b9dbb94835238f0171af1f58647
ehoogsto <evelinekeh@gmail.com>
2015-04-21 10:28:45 +0200

    height of drone

---
+++ js/components/lineChart.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Eveline on 21/04/2015.
+ */
+App.LiveGraphComponent = Ember.Component.extend({
+
+    draw: function(){
+        var data = [{ label: 'A', values: [] }];
+        var chart = $('#lineChart').epoch({
+            type: 'time.line',
+            data: data,
+            className : "category10",
+            axes: ['right', 'bottom']
+        });
+        this.set('chart',chart);
+
+        this.socketManager.register("altitudeChanged", this.get("id"), "drone", function(data) {
+            var entry = [];
+            var timestamp = Date.now() / 1000 | 0;
+            entry.push({ time: timestamp, y: data.altitude.toFixed(2) });
+            console.log('DATA: ' + timestamp + " " + data.altitude.toFixed(2));
+            chart.push(entry);
+        });
+    }.on('didInsertElement')
+
+});
+
--- js/components/liveGraph.js
+++ js/components/liveGraph.js
@@ -1,14 +1,27 @@
 App.LiveGraphComponent = Ember.Component.extend({
+    start : 1,
+    x : 0,
+
 	show: function() {
+        var data = new RealTimeData(2);
 
-  var data = new RealTimeData(2);
+        var chart = $('#gauge')
+            .epoch({
+                type: 'time.gauge',
+                value: 1.0,
+                tickOffset: 20
+        });
+        var self = this;
+        window.setInterval(function() {
+            var x = self.get('x')+0.1;
+            if(x >= 1.0){//reset
+                self.set('x', 0);
+            }else {
+                self.set('x', x);
+            }
+            var y = self.get('start')-x;
 
-   var chart = $('#lineChart')
-   .epoch({
-        type: 'time.gauge',
-		value: 0.5,
-		tickOffset: 20
-    });
-    window.setInterval(function() { var x = Math.random(); console.log(x); chart.update(x); }, 1000);
-	}.on('didInsertElement'),
+            chart.update(y);
+        }, 1000)
+	}.on('didInsertElement')
 });
