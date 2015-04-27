976375d5a27eeb217413826d3dabb60b70591416
Matthias Verstraete <mmaverst.verstraete@ugent.be>
2015-04-27 17:59:24 +0200

    fill in correct bounds

--- js/components/maps/basestationAddMap.js
+++ js/components/maps/basestationAddMap.js
@@ -13,24 +13,6 @@ App.BasestationAddMapComponent = App.BasestationMapComponent.extend({
                 self.set('location', Ember.Object.create({lat : e.latlng.lat, lon : e.latlng.lng}));
             });
         });
-    },
-
-    updateMap : function() {
-        Ember.run.once(this, 'updateFunction');
-    },
-
-    updateFunction : function() {
-        var loc = this.get('location');
-        var map = this.get('map');
-        if (map)
-            if (! loc)
-                map.setView([0,0],1);
-            else if (loc[0] instanceof Array)
-                map.fitBounds(loc, {padding:[50,50]});
-            else {
-                var zoom = map.getZoom() || 13;
-                map.setView(loc, zoom);
-            }
     }
 
 });
