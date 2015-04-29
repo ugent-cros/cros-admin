ca09bbe509d85a5a7d8ba7ce8d8011a490e75fc7
ehoogsto <evelinekeh@gmail.com>
2015-04-29 13:33:06 +0200

    styling gauge and line chart

--- js/components/charts/lineChart.js
+++ js/components/charts/lineChart.js
@@ -4,21 +4,24 @@
 App.LineGraphComponent = Ember.Component.extend({
 
     draw: function(){
-        var data = [{ label: 'A', values: [] }];
-        var chart = $('#lineChart').epoch({
-            type: 'time.line',
-            data: data,
-            className : "category10",
-            axes: ['right', 'bottom']
-        });
-        this.set('chart',chart);
+        var self = this
+        $('.modal').on('shown.bs.modal', function (e) {
+            var data = [{ label: 'A', values: [] }];
+            var chart = $('#lineChart').epoch({
+                type: 'time.line',
+                data: data,
+                className : "category10",
+                axes: ['right', 'bottom']
+            });
+            self.set('chart',chart);
 
-        this.socketManager.register("altitudeChanged", this.get("drone"), "linegraph", function(data) {
-            var entry = [];
-            var timestamp = Date.now() / 1000 | 0;
-            entry.push({ time: timestamp, y: data.altitude.toFixed(2) });
-            //console.log('DATA: ' + timestamp + " " + data.altitude.toFixed(2));
-            chart.push(entry);
+            self.socketManager.register("altitudeChanged", self.get("drone"), "linegraph", function(data) {
+                var entry = [];
+                var timestamp = Date.now() / 1000 | 0;
+                entry.push({ time: timestamp, y: data.altitude.toFixed(2) });
+                //console.log('DATA: ' + timestamp + " " + data.altitude.toFixed(2));
+                chart.push(entry);
+            });
         });
     }.on('didInsertElement')
 
