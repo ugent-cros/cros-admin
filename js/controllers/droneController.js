54e21a6849e71bc66bad3cb87074cf39a42d1629
Matthias Verstraete <mmaverst.verstraete@ugent.be>
2015-04-30 21:55:27 +0200

    add emergency button

--- js/controllers/droneController.js
+++ js/controllers/droneController.js
@@ -35,6 +35,10 @@ App.DroneController = Ember.ObjectController.extend({
             self.set('location',Ember.Object.create({lat : data.latitude, lon : data.longitude}));
             // todo: do something with gpsheight...?
         });
+
+        this.socketManager.register("droneStatusChanged", this.get("model.id"), "drone", function(data) {
+            self.set('model.status',data.newStatus);
+        });
     }.observes("model"),
 
     automatic : function() {
@@ -66,9 +70,24 @@ App.DroneController = Ember.ObjectController.extend({
             return label + "label-default";
         else if(status == "CHARGING")
             return label + "label-primary";
-        else if(status == "EMERGENCY_LANDED" || status == "DECOMMISSIONED")
+        else if(status == "EMERGENCY" || status == "DECOMMISSIONED")
             return label + "label-warning";
     }.property('status'),
+
+    getModelClass: function(){
+        var label = "label "
+        var status = this.get('model.status');
+        if(status == "AVAILABLE")
+            return label + "label-success";
+        else if(status == "IN_FLIGHT")
+            return label + "label-info";
+        else if(status == "UNAVAILABLE" || status == "UNKNOWN")
+            return label + "label-default";
+        else if(status == "CHARGING")
+            return label + "label-primary";
+        else if(status == "EMERGENCY" || status == "DECOMMISSIONED")
+            return label + "label-warning";
+    }.property('model.status'),
 
     controlError : "",
     hasControlError : function() {
@@ -91,6 +110,12 @@ App.DroneController = Ember.ObjectController.extend({
                 self.set("model.status", "MANUAL_CONTROL");
             }, function(data) {
                 self.set("controlError", data.responseJSON);
+            });
+        },
+
+        emergency: function(id){
+            this.adapter.find('drone',id,"emergency").then(function(data){
+                console.log(data);
             });
         }
     }
